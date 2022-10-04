31443798e2239d30b651c0275c6a4796fa5ec6a0
gray-water <minyung0304@gmail.com>
2022-10-04 10:56:33 +0900

    플레이어_3-1
    Hit 스크립트와 Status 스크립트에 내용 추가

--- Assets/02.Scripts/Entity/Player/PlayerStatus.cs
+++ Assets/02.Scripts/Entity/Player/PlayerStatus.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class PlayerStatus : Actor
 {
+    static public int Player_HP;
+
+    public GameObject Player;
 
     void Start()
     {
-        
+        Player_HP = hp_;
     }
 
 
     void Update()
     {
-        
+        if (Player_HP <= 0)
+        {
+            Debug.Log("Player Dead");
+            Destroy(Player);
+        }
     }
 }
