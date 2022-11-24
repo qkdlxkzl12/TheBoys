69fd287abedbe3bce0f72a4a3e3c83cc4344cd5e
gray_water <minyung0304@gmail.com>
2022-11-24 22:02:58 +0900

    작업 내용 일부 복구
    버프 복구
    
    탄창 이펙트 미복구
    
    서브 브런치 활성

---
+++ Assets/02.Scripts/Entity/Player/BuffManager.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public enum Buffs {Illusion, }
+
+public class BuffManager : MonoBehaviour
+{
+    IEnumerator Illu;
+
+    public void DeBuff(Buffs Type,  float Times)
+    {
+
+        switch(Type)
+        {
+            case Buffs.Illusion:
+                illusion();
+                break;
+
+        }
+
+        void illusion()
+        {
+            GameManager.instance.player.GetComponent<PlayerGlobal>().Buff_Illusion = true;
+
+            if(Illu != null)
+            {
+                StopCoroutine(Illu);
+                Illu = null;
+                Debug.Log("환각 초기화");
+            }
+
+            Illu = Ilus();
+            StartCoroutine(Illu);
+            Debug.Log("환각 시작");
+            
+           IEnumerator Ilus()
+            {
+                yield return new WaitForSeconds(Times);
+                GameManager.instance.player.GetComponent<PlayerGlobal>().Buff_Illusion = false;
+                Debug.Log("환각 종료");
+                Illu = null;
+            }
+
+
+        }
+
+        
+    }
+    
+
+}
