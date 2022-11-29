bdad00f364f29686e370cf32d8813891f6031c0d
gray-water <minyung0304@gmail.com>
2022-11-29 11:54:29 +0900

    플레이어 사격 시스템 Ver.0
    기본적인 사격 시스템의 틀을 잡아둠
    
    총알 이동 필요

--- Assets/02.Scripts/Entity/Bullet/PlayerBullet.cs
+++ Assets/02.Scripts/Entity/Bullet/PlayerBullet.cs
@@ -2,24 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerBullet : MonoBehaviour
+public class PlayerBullet : Bullet
 {
-    [SerializeField]
-    Element element;
-    BulletType type;
+    public Element element;
+    public BulletType type;
+
+    //public Sprite
+
     Vector2 direction;
     float value;
+
+    float DeadTime = 15f;
 
     //public EnemyBullet(int hp, int attackDamage, int moveSpeed, float attackDelay) : base(hp, attackDamage, moveSpeed, attackDelay)
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+
     }
 
     public void Init(Element element)
