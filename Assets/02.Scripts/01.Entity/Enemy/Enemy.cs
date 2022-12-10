c40dc8f3ce3460ebd8c05ae7b199f51346e34084
gray_water <minyung0304@gmail.com>
2022-12-10 14:59:07 +0900

    일반몹 Ver.2
    - 까마귀 및 원숭이 용 공격 형식 추가
    
    - 애니메이션 전원 추가 완료

--- Assets/02.Scripts/01.Entity/Enemy/Enemy.cs
+++ Assets/02.Scripts/01.Entity/Enemy/Enemy.cs
@@ -14,6 +14,8 @@ public class Enemy : Actor
     public float AttackSpeed; //공격 속도
     public float MoveLoadingTime; //이동 및 공격 전 대기 시간
     public bool Can_Act = false; //이동 공격 가능 여부 
+    public bool AttackTypeRaven = false; //까마귀 여부
+    public bool AttackTypeLazoul = false;
 
     float onAttackingTime = 0.6f; //대기 모션 전환 시간
 
@@ -40,10 +42,20 @@ public class Enemy : Actor
         Invoke("TurnOn", MoveLoadingTime);
     }
 
+    private void Update()
+    {
+        if(FireStack >= 3) //화상 폭발
+        {
+
+        }
+    }
+
     private void FixedUpdate()
     {
         if(Can_Act)
+        {
             MovingOn();
+        }
     }
 
    void MovingOn()
@@ -52,7 +64,7 @@ public class Enemy : Actor
 
         Me.transform.Translate((Player.transform.position.normalized - Me.transform.position.normalized) * MoveSpeed * Time.deltaTime);
 
-        if(Player.transform.position.x - Me.transform.position.y <= 0)
+        if(Player.transform.position.x - Me.transform.position.x <= 0)
         {
             Me.GetComponent<SpriteRenderer>().flipX= false;
         }   
@@ -61,6 +73,12 @@ public class Enemy : Actor
             Me.GetComponent<SpriteRenderer>().flipX = true;
         }
 
+        if(AttackTypeRaven)
+            if(Math.Abs(Player.transform.position.x - Me.transform.position.y) <= 15)
+            {
+                Can_Act = false;
+                RushOn();
+            }
     }
 
     void AttackOn()
@@ -69,12 +87,15 @@ public class Enemy : Actor
         {
             yield return new WaitForSeconds(AttackSpeed);
 
-            Vector3 d = Player.transform.position - Me.transform.position;
-            float angle = Mathf.Atan2(d.y, d.x) * Mathf.Rad2Deg;
+            if(Can_Act)
+            {
+                Vector3 d = Player.transform.position - Me.transform.position;
+                float angle = Mathf.Atan2(d.y, d.x) * Mathf.Rad2Deg;
 
-            Instantiate(Knives, new Vector3(Me.transform.position.x, Me.transform.position.y, Me.transform.position.z), Quaternion.AngleAxis(angle - 90, Vector3.forward));
+                Instantiate(Knives, new Vector3(Me.transform.position.x, Me.transform.position.y, Me.transform.position.z), Quaternion.AngleAxis(angle - 90, Vector3.forward));
 
-            Attack_Motion();
+                Attack_Motion();
+            }
 
             Invoke("Attack_Motion", onAttackingTime);
             StartCoroutine(Shot());
@@ -85,17 +106,49 @@ public class Enemy : Actor
 
     void Attack_Motion()
     {
-        if(Anime.GetBool("onAttack") == false)
-            Anime.SetBool("onAttack", true);
-        else
-            Anime.SetBool("onAttack", false);
+        if(AttackTypeRaven == false)
+        {
+            if (Anime.GetBool("onAttack") == false)
+                Anime.SetBool("onAttack", true);
+            else
+                Anime.SetBool("onAttack", false);
+        }
+    }
 
+    void RushOn()
+    {
+        Anime.SetBool("onRush", true);
+        Me.transform.Translate(Player.transform.position * Time.deltaTime);
+
+       IEnumerator RushStop()
+        {
+            yield return new WaitForSeconds(0.5f);
+            Anime.SetBool("onRush", false);
+            Can_Act = true;
+        }
+
+        StartCoroutine(RushStop());
+    }
+
+    void Dead_Motion()
+    {
+        if(AttackTypeLazoul == false)
+        {
+            Anime.SetBool("onDead", true);
+        }
     }
 
     void TurnOn()
     {
         AttackOn();
+            
         Can_Act = true;
+    }
+
+    override protected void OnDie()
+    {
+        Debug.Log(gameObject.name + "Die");
+        Destroy(gameObject);
     }
 
 }
